da2b172ae40af90788bcaa980bc6f62d399c76f5
inglkruiz <ing.lkruiz@gmail.com>
2020-10-31 14:44:09 +0100

    Adds ts library to share TS types

---
+++ libs/ts/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'ts',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/ts',
+};
